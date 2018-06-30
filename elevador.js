9b756a721533853ca1591b455942fd9361e8ad89
paulinaALV <velezpaulina139@gmail.com>
2018-06-30 10:19:30 -0500

    hola

--- elevador.js
+++ elevador.js
@@ -1,14 +1,26 @@
 var piso_seleccionado = 18;
-var piso_actual = 1;
-var direccion = prompt ("subes o bajas");
+var piso_actual= 1;
+var direccion = prompt ('subes o bajas?');
 const primer_piso = 2;
 const ultimo_piso = 20;
 
-var piso_seleccionado = prompt ("ingrese el piso");
-if (direccion == "subes");
-for(piso_actual; piso_actual <= ultimo_piso; piso_actual++) {
-	console.log ("estas en el piso" + piso_actual);
-		if (piso_actual == piso_seleccionado){
-			alert ("llegaste");
-		}
+var piso_seleccionado = prompt ('a que piso vas?');
+if (direccion == "subes") {
+for (piso_actual; piso_actual <= ultimo_piso ; piso_actual++){
+    console.log ("estas en el piso" + piso_actual);
+    if (piso_actual == piso_seleccionado){
+        alert ("Llegaste");
+    }
 }
+}
+
+
+ if (direccion == "bajas") {
+for (piso_actual= 20;piso_actual >= primer_piso; piso_actual--){
+    console.log ("estas en el piso" + piso_actual);
+    if (piso_actual == piso_seleccionado){
+        alert ("Llegaste");
+    }
+}
+}
+
