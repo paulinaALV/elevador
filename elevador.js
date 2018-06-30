e1696e32c6cd07a19cb0ddd8e656b5377d806d8b
paulinaALV <velezpaulina139@gmail.com>
2018-06-30 09:36:30 -0500

    hola

---
+++ elevador.js
@@ -0,0 +1,14 @@
+var piso_seleccionado = 18;
+var piso_actual = 1;
+var direccion = prompt ("subes o bajas");
+const primer_piso = 2;
+const ultimo_piso = 20;
+
+var piso_seleccionado = prompt ("ingrese el piso");
+if (direccion == "subes");
+for(piso_actual; piso_actual <= ultimo_piso; piso_actual++) {
+	console.log ("estas en el piso" + piso_actual);
+		if (piso_actual == piso_seleccionado){
+			alert ("llegaste");
+		}
+}
